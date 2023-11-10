382eda8ba9d7ff39acdb2bfe2f2fbf82b93da7e1
Olesya Figurovskaya <lisonkadrug@gmail.com>
2023-11-10 16:33:00 +0300

    style cardProducts rubber

--- vue3-shop/src/components/BreadCrumbsCard.vue
+++ vue3-shop/src/components/BreadCrumbsCard.vue
@@ -11,7 +11,7 @@ export default {
 </script>
 
 <template>
-<div class="breadcrumbsCard mb-8">
+<div class="breadcrumbsCard mb-25">
             <router-link to="/" class="breadcrumbs__main">Главная</router-link>
             <span> — </span>
             <router-link :to=this.path class="breadcrumbs__products breadcrumbs__main">Магазин</router-link>
